d4fd70bcfcc920d1dedfbebf0b374424e6de43a5
Kate-Pavlova <KatrinaOnligm@gmail.com>
2022-09-24 11:31:43 +0300

    Изм.2

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 console.log("Hello Word!");
 
+const ourPlanetName = "Земля";
 
-const ourPlanetName= "Земля";
-let userCurrentTime= 12.30;
-let numberArticles= "Количество статей равно 23";
-let paymentCashOrCard= "Нал или Безнал";
-let nameUser= "Катя";
-let surnameUser= "Павлова";
-let patronUser= "Дмитриевна";
+let userCurrentTime = 12.3;
+let numberArticles = "Количество статей равно 23";
+let paymentCashOrCard = "Нал или Безнал";
+let nameUser = "Катя";
 
-console.log(ourPlanetName,userCurrentTime,numberArticles,paymentCashOrCard,nameUser,surnameUser,patronUser);
+let surnameUser = "Павлова";
+let patronUser = "Дмитриевна";
 
+console.log(
+  ourPlanetName,
+  userCurrentTime,
+  numberArticles,
+  paymentCashOrCard,
+  nameUser,
+  surnameUser,
+  patronUser
+);
