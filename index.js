bcd726615d187c98c8ff416b191cf5c4c9181036
Kate-Pavlova <KatrinaOnligm@gmail.com>
2022-09-24 10:37:37 +0300

    добавил файлф первого занятия

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello Word!");
+
+
+const ourPlanetName= "Земля";
+let userCurrentTime= 12.30;
+let numberArticles= "Количество статей равно 23";
+let paymentCashOrCard= "Нал или Безнал";
+let nameUser= "Катя";
+let surnameUser= "Павлова";
+let patronUser= "Дмитриевна";
+
+console.log(ourPlanetName,userCurrentTime,numberArticles,paymentCashOrCard,nameUser,surnameUser,patronUser);
+
