7ce981e6e0f7423c130ce14ccbf3747938d4f1ca
Kate-Pavlova <KatrinaOnligm@gmail.com>
2022-10-01 12:43:12 +0300

    Урок по преобр. типов и опер. сравнения (01.10.22)

---
+++ lesson2.js
@@ -0,0 +1,93 @@
+/*const name = "Kate";
+
+
+const nameTonumber = Number(name);
+console.log(nameTonumber);
+
+const age = 26;
+const ageTostring = String(age);
+console.log(typeof ageTostring);
+
+const nullValue = null;
+console.log (Number(nullValue));
+
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean({}));
+
+
+console.log(typeof +"45");
+console.log(typeof Number("45"));
+
+console.log(2 == 3);
+console.log(2 == 2);
+console.log(2 < 3);
+console.log(2 > 3);
+
+console.log(2 + 2 + "3");
+console.log("2" + 3 + 3);
+console.log("2" + "3");
+console.log(2 + 3);
+
+
+console.log(typeof ("3" -"15"));
+
+console.log(2===2);
+
+console.log(5 > 2);
+console.log("ананас" > "яблоко");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log(7 / 0);
+console.log("  -9" + 5);
+console.log("-9" - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n" - 2);
+
+const towns = ["Moskow","Paris","London"];
+console.log(typeof {});
+console.log(typeof []);
+console.log(Array.isArray({})); // false
+console.log(Array.isArray([])); // true 
+
+const towns = ["Moskow", "Paris", "London", "NN"];
+console.log(towns);
+towns[1] = "Milan";
+console.log(towns);
+
+towns[4] = "St.Peterburg";
+console.log(towns);
+
+towns[9] = "Berlin";
+console.log(towns);
+const example = [];
+example[100]="Example";
+console.log(example.length);
+
+const towns = ["Moskow", "Paris", "London", "NN"];
+console.log(towns);
+towns[1] = "Milan";
+console.log(towns);
+towns.push("St.Peterburg");
+towns[towns.length - 1]="Orel";
+console.log(towns);*/
+
+const films = ["Film1", "Film2", "Film3", "Film4", "Film5"];
+console.log(films[0]);
+films.push("Film6");
+console.log(films);
+console.log(films[films.length - 1]);
