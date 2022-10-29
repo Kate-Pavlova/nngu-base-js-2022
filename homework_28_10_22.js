5f3be93829a85859e771f254e33280a0a3ff7393
Kate-Pavlova <KatrinaOnligm@gmail.com>
2022-10-29 09:02:18 +0300

    1 изм домашки

--- homework_28_10_22.js
+++ homework_28_10_22.js
@@ -25,7 +25,7 @@ let str = "а может быть сможем";
 function verhniRegistr(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
-console.log((verhniRegistr(str)));*/
+console.log((verhniRegistr(str)));
 
 
 
@@ -34,3 +34,52 @@ function capitalize(str) {
  return str.replace(/(^|\s)\S/g, function(Slovo) {return Slovo.toUpperCase()})
 }
 console.log((capitalize(str)))
+
+
+
+let str = "КаЖдЫй ОхОтНиК жЕлАеТ зНаТь";  
+ 
+function changeRegister(str) {
+let newStr = "";
+let lenStr = str.length;
+  for (i = 0; i < lenStr; i++) {
+    if (str[i] === str[i].toLowerCase()) {
+      newStr += str[i].toUpperCase();
+    } else {
+      newStr += str[i].toLowerCase();
+    }
+  }
+  return newStr;
+}
+console.log(changeRegister(str))
+
+let str = " hello., -/ world # ;: } $ % ^ & *" 
+function removeChar(str) {
+   let res = "";
+   res = str.replace(/[^\w\s]|_/g, "")
+          .replace(/\s+/g, " ");
+   return res;      
+}
+console.log(removeChar(str))
+
+
+function zeros (num, len,sign) {
+    let len_zer = len - (num + '').length, // к-во нулей
+    num_zer = '';
+      for ( ; len_zer > 0; len_zer--) {
+        num_zer += 0;
+      }
+        
+      return (sign === undefined || sign === '') ? num_zer + num + '' : sign + num_zer + num +'';
+    }
+    console.log(zeros (33, 4,"-"))*/
+
+
+
+
+    function comparison(str1, str2) {
+        let res = str1.toUpperCase() === str2.toUpperCase();
+      
+        return res;
+      }
+      console.log(comparison("string", "StRing"))
